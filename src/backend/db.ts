b7a3c1133df85bc23564d590b51ac0bfd5963169
CMS2814 <cydhensabate14@gmail.com>
2026-09-03 23:35:57 -0300

    es6-string-html addition

--- src/backend/db.ts
+++ src/backend/db.ts
@@ -1,20 +1,17 @@
-/*sql*/
-import * as types from "../shared/types.js";
-
 import Database from "better-sqlite3";
 
 const db = new Database("app.db"); //add for debugging{ verbose: console.log }
 
 db.pragma("journal_mode = WAL");
 
-db.exec(`CREATE TABLE IF NOT EXISTS messages(
+db.exec(/*sql*/ `CREATE TABLE IF NOT EXISTS messages(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id TEXT NOT NULL,
     content TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 )`);
 
-const pruneStmt = db.prepare(`
+const pruneStmt = db.prepare(/*sql*/ `
         DELETE FROM messages 
         WHERE id IN (
           SELECT id FROM messages 
@@ -24,15 +21,18 @@ const pruneStmt = db.prepare(`
       `);
 
 const insertStmt = db.prepare(
-  "INSERT INTO messages (user_id, content) VALUES (?,?)",
+  /*sql*/ `INSERT INTO messages (user_id, content) VALUES (?,?)`,
 );
-export const saveMessage = db.transaction((userId: string, content: string) => {
-  insertStmt.run(userId, content);
-  pruneStmt.run();
-});
+
+export const saveMessage = db.transaction(
+  /*sql*/ (userId: string, content: string) => {
+    insertStmt.run(userId, content);
+    pruneStmt.run();
+  },
+);
 
 export function getMessageLog(limit: number = 100) {
-  const initMessageLog = db.prepare(`
+  const initMessageLog = db.prepare(/*sql*/ `
       SELECT id, user_id AS username, content AS message 
       FROM (
         SELECT id, user_id, content 
